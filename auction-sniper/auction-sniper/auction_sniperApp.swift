b78700bb5c2290b1ce38377403636f9a06aebcbb
David Lam <davidlys@gmail.com>
2020-11-15 08:18:05 +0800

    create auction-sniper project

---
+++ auction-sniper/auction-sniper/auction_sniperApp.swift
@@ -0,0 +1,17 @@
+//
+//  auction_sniperApp.swift
+//  auction-sniper
+//
+//  Created by David on 15/11/20.
+//
+
+import SwiftUI
+
+@main
+struct auction_sniperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
